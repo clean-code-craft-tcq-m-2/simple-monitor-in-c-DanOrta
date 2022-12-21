81023fbc93b7e6c0cab2ec77eb385991af822f5c
Daniel Orta <daniel.orta.uaslp@gmail.com>
2022-12-20 20:00:48 -0600

    Corrected the issue in which the function 'PrintMessageIfError' was not printing all messages.

--- main.c
+++ main.c
@@ -3,19 +3,18 @@
 #include "checkBatteryStatus.h"
 #include "printMessages.h"
 
-
 int main(void)
 {
 #if 0
-  assert(GetBatteryStatus(25, 70, 0.7));
-  assert(!GetBatteryStatus(50, 85, 0));
+	assert(GetBatteryStatus(25, 70, 0.7));
+	assert(!GetBatteryStatus(50, 85, 0));
 #else
-  printf("%u\n", GetBatteryStatus(25.0, 70.0, 0.7));
-  printf("%u\n", GetBatteryStatus(50.0, 85.0, 0.0));
-  printf("%u\n", GetBatteryStatus(50.0, 85.0, 1.0));
+	GetBatteryStatus(25.0, 70.0, 0.7);
+	GetBatteryStatus(50.0, 85.0, 0.0);
+	GetBatteryStatus(50.0, 85.0, 1.0);
 
-  PrintMessageIfError((uint8_t)0x07U, SPANISH);
+	PrintMessageIfError((uint8_t)0x07U, SPANISH);
 #endif
 
-  return 0;
+	return 0;
 }
